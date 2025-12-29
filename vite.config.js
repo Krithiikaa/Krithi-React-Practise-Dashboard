ff178fd4936272bcb1309f4c6a66af688754576b
Krithiikaa <krithikaarajkumaar@gmail.com>
2025-12-30 01:59:42 +0530

    Fix base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: "Krithi-React-Practise-Dashboard", // MUST match your repo name
-=======
-  base: "/Krithi-React-Practise-Dashboard/", // MUST match your repo name
->>>>>>> cbcd077f628e9ef129860fe6db84d68e3472b947
+  base: "/Krithi-React-Practise-Dashboard/",
 });
